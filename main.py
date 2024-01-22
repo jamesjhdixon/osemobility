77111c0f7a790fbd85e3d6982d7247822871d6e7
James Dixon <36702681+jamesjhdixon@users.noreply.github.com>
2024-01-22 17:19:18 +0000

    cleanup

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ James Dixon, University of Strathclyde
 james.dixon@strath.ac.uk
 """
 
+from modules.vehicle_stock.tech_choice.tech_choice import do_something
+
+print(do_something())
---
+++ modules/vehicle_stock/tech_choice/tech_choice.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# get technology table (csv)
+def get_technology():
+    return pd.read_csv(f"./modules/vehicle_stock/tech_choice/data/technology.csv")
+
+def do_something():
+    p = get_technology()
+    return p.head()
