f713ae6618b3e393b313a4cc5c8c3e5c3c225b9d
James Dixon <36702681+jamesjhdixon@users.noreply.github.com>
2024-05-02 11:59:40 +0100

    venv

--- modules/vehicle_stock/tech_choice/tech_choice.py
+++ modules/vehicle_stock/tech_choice/tech_choice.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+
 # get technology table (csv)
 def get_technology():
     return pd.read_csv(f"./modules/vehicle_stock/tech_choice/data/technology.csv")
+
 
 def do_something():
     p = get_technology()
